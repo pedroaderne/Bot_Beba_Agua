bdcd40189576c03b92301b2f01a11b594a48d900
pedroaderne <pedro.aderne20@gmail.com>
2021-09-25 02:07:23 -0300

    atualizando gemfile

--- bot.rb
+++ bot.rb
@@ -1,21 +1,22 @@
 require 'discordrb'
+require 'dotenv/load'
 require_relative 'lembra'
 
 classe = MeLembra.new()
 
 bot = Discordrb::Commands::CommandBot.new token: 
-'ODkxMDc2ODU0Mjg1NDg0MDUy.YU5F_Q.8BETY-LwcuVhq-zUbucykTjFMTQ', prefix: '='
+ENV['TOKEN'], prefix: '='
 
-bot.command( :lembrete, max_args: 1 ) do |_event, *args|
+bot.command( :lembrete, max_args: 1 ) do |event, *args|
 
   command = "#{ args.at(0).downcase }"
 
-  resp = classe.lembrete(command, _event.user)
+  resp = classe.lembrete(command, event.user)
 
   if resp == 0
-    _event.respond 'Koe, bora beber agua!'
+    event.respond 'Koe, bora beber agua!'
   elsif resp == 1 
-    _event.respond 'So nao esquece de beber agua!'
+    event.respond 'So nao esquece de beber agua!'
   end
 
 end
